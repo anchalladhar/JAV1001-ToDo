aa3ce87bfed0ed666e966d4ce78aa6b4a3b10b7a
mahektailor <57285513+mahektailor@users.noreply.github.com>
2023-07-13 21:31:43 -0400

    Added Button Connection via private variable

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.todoapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.CheckedTextView
+import android.widget.EditText
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    // Added Button connection
+    private lateinit var editTextTask: EditText
+    private lateinit var buttonAddTask: Button
+    private lateinit var listViewTasks: ListView
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextTask = findViewById(R.id.editTextTask)
+        buttonAddTask = findViewById(R.id.buttonAddTask)
+        listViewTasks = findViewById(R.id.listViewTasks)
+
     }
+
+
 }
